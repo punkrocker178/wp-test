3c5e3de5f2e563c7595d0d43ddcebd051b3f1445
steve.le <steve.le@contemi.com.vn>
2020-01-07 12:33:16 +0700

    Following up wp tutorial Add scripts, styles to wordpress

--- wp-content/themes/steve-theme/page.php
+++ wp-content/themes/steve-theme/page.php
@@ -1,10 +1,12 @@
 <?php get_header();
+
     while(have_posts()) {
         the_post(); ?>
+        <h2>This is a Page</h2>
         <h2>
             <?= the_title(); ?>
         </h2>
         <p><?= the_content(); ?></p>
 <?php
-get_footer();
+    get_footer();
 }
