dc047bf29a36bf1b30a8f4c520977cb5a6e12bf9
punkrocker178 <ngochieu.le1708@gmail.com>
2020-02-19 08:03:57 +0700

    Video 21 22

--- wp-content/themes/steve-theme/front-page.php
+++ wp-content/themes/steve-theme/front-page.php
@@ -43,27 +43,27 @@
     <div class="full-width-split__two">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+        <?php
 
+          $homepagePosts = new WP_Query(array(
+            'posts_per_page' => 2
+          ));
+          while($homepagePosts->have_posts()) {
+            $homepagePosts->the_post(); ?>
         <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Jan</span>
-            <span class="event-summary__day">20</span>  
+            <span class="event-summary__month"><?= the_time('M'); ?></span>
+            <span class="event-summary__day"><?= the_time('d'); ?></span>  
           </a>
           <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-            <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?= the_permalink() ?>"><?= the_title(); ?></a></h5>
+            <p><?php echo wp_trim_words(get_the_content()); ?> <a href="<?= the_permalink() ?>" class="nu gray">Read more</a></p>
           </div>
         </div>
-        <div class="event-summary">
-          <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Feb</span>
-            <span class="event-summary__day">04</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-            <p>Two of our processors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-          </div>
-        </div>
+        <?php 
+          }
+          wp_reset_postdata();
+        ?>
         
         <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
